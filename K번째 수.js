da7166406a02a4241bc56b4ef3cf90519ab2a8ef
hoontail <sper456@naver.com>
2022-03-25 11:04:24 +0900

    3.25

---
+++ K번째 수.js
@@ -0,0 +1,15 @@
+function solution(array, commands) {
+    var answer = [];
+    for(let i = 0; i < commands.length; i ++){ 
+        let com = commands[i][2] -1  
+        let arr = array.slice(commands[i][0]-1, commands[i][1])
+        arr.sort(function(a, b){
+         return a- b
+             
+    }) 
+       answer.push(arr[com])
+      
+        
+ 
+}
+  return answer}
